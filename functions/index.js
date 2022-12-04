e1a430effd49753ab6746fb2706126d6a40c0e36
thepnatee <thepnatee@emetworks.com>
2022-12-04 22:59:36 +0700

    update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
 const util = require('./util');
 const flex = require('./flex');
-const {
-    initializeApp,
-    cert
-} = require('firebase-admin/app');
+const {  initializeApp,  cert } = require('firebase-admin/app');
 const {
     getFirestore
 } = require('firebase-admin/firestore');
@@ -30,11 +27,19 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
             }
 
 
+            //  Start Join with LINE Group 
+            if (event.type === "join") {
+                if (event.source.type === "group") {
+                    await util.reply(event.replyToken, [flex.welcomeMessage()]);
+                }
+            }
+
+            
             if (event.type === "message") {
 
-                
+
                 if (event.source.type === "group") {
-                    // insert and update request groupId
+                    // insert and update member by groupId
                     insertUserGroup(event.source.userId, event.source.groupId)
                 }
 
@@ -42,9 +47,9 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
                 if (event.message.type === "text") {
 
                     let textMessage = event.message.text
-                  
+
                     if (textMessage === "สวัสดี") {
-                        await util.reply(event.replyToken, [flex.quickReplyWelcomeMessage()]);
+                        await util.reply(event.replyToken, [flex.welcomeMessage()]);
                     }
 
                     if (textMessage === "ตี้ฉัน") {
@@ -53,24 +58,25 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
                             type: "text",
                             text: "ตอนนี้ ตี้ของคุณมี " + countGroup + " คน แบ่งกันให้ถูกนะ!"
                         }]);
-                        // await util.reply(event.replyToken, [flex.quickReplyWelcomeMessage()]);
+                    }
+
+
+                    if (textMessage === "แตกตี้") {
+                        await util.reply(event.replyToken, [{
+                            type: "text",
+                            text: "ให้พิมพ์คำแรกว่า แตก ตามด้วย {{จำนวนคน ตามจำนวนโต๊ะ}} เช่น\n\nแตก 4 4\n==> หมายถึง มี 2 โต๊ะ โต๊ะละ 4 คน\nแตก 2 4 4\n==> หมายถึง มี 3 โต๊ะ\n       โต๊ะที่ 1 จำนวน 2 คน\n       โต๊ะที่ 2 จำนวน 4 คน\n       โต๊ะที่ 3 จำนวน 4 คน\n\nข้อจำกัด\n1.ต้องไม่เกินจำวนคนในโต๊ะ \n2. โต๊ะต้องไม่ต่ำกว่า 2 คน และไม่เกิน 10-20 คน\n3.แบ่งโต๊ะได้ไม่เกิน 10 โต๊ะ"
+                        }]);
                     }
 
                     // ----------------------- main service 
                     let splitStringMessage = textMessage.split(' ')
                     let subStringMessage = splitStringMessage[0].substring(0, 4)
-                    if (textMessage === "แตกตี้") {
-                        await util.reply(event.replyToken, [{
-                            type: "text",
-                            text: "ให้พิมพ์คำแรกว่า แตก ตามด้วย {{จำนวนคน ตามจำนวนโต๊ะ}} เช่น\n\nแตก 4 4\n==> หมายถึง มี 2 โต๊ะ โต๊ะละ 4 คน\nแตก 2 4 4\n==> หมายถึง มี 3 โต๊ะ\n       โต๊ะที่ 1 จำนวน 2 คน\n       โต๊ะที่ 2 จำนวน 4 คน\n       โต๊ะที่ 3 จำนวน 4 คน\n\nข้อจำกัด\n1.ต้องไม่เกินจำวนคนในโต๊ะ \n2. โต๊ะต้องไม่ต่ำกว่า 2 คน และไม่เกิน 10-15 คน\n3.แบ่งโต๊ะได้ไม่เกิน 10 โต๊ะ"
-                        }]);
-                    }
-
                     if (subStringMessage === "แตก") {
-                        let arrLoopFlex = []
-                        let sumUserCountInloop = 0
                         let countGroup = await countUserGroup(event.source.groupId)
                         if (splitStringMessage.length > 2 && countGroup >= 2) {
+
+                            let arrLoopTable = []
+                            let sumNumMember = 0
                             splitStringMessage.forEach(async elm => {
 
                                 // Check is not แตก
@@ -84,15 +90,15 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
 
                                         // Check Type Number and Validate Nan
                                         if (typeof countNumber === 'number' && countNumber === countNumber) {
-                                            sumUserCountInloop += parseInt(countNumber);
-                                            arrLoopFlex.push(countNumber)
+                                            sumNumMember += parseInt(countNumber);
+                                            arrLoopTable.push(countNumber)
                                         } else {
                                             await util.reply(event.replyToken, [{
                                                 type: "text",
                                                 text: "Format แตกไม่ถูกต้อง กรุณาพิมพ์ แตก เว้นวรรค {ตัวเลขจำนวนคน} ในแต่ละโต๊ะ\nให้พิมพ์คำแรกว่า แตก ตามด้วย {{จำนวนคน ตามจำนวนโต๊ะ}} เช่น\n\nแตก 4 4\n==> หมายถึง มี 2 โต๊ะ โต๊ะละ 4 คน\nแตก 2 4 4\n==> หมายถึง มี 3 โต๊ะ\n       โต๊ะที่ 1 จำนวน 2 คน\n       โต๊ะที่ 2 จำนวน 4 คน\n       โต๊ะที่ 3 จำนวน 4 คน\n\nข้อจำกัด\n1.ต้องไม่เกินจำวนคนในโต๊ะ \n2.โต๊ะต้องไม่ต่ำกว่า 2 คน และไม่เกิน 10 คน\n3.แบ่งโต๊ะได้ไม่เกิน 10 โต๊ะ"
                                             }]);
                                         }
-                                    }else{
+                                    } else {
                                         await util.reply(event.replyToken, [{
                                             type: "text",
                                             text: "ห้ามพิมพ์ 0 นะ เอาดีๆ เดี๋ยวหน้า แตกหรอก"
@@ -104,16 +110,16 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
 
                             });
 
-                            console.log("countGroup :", countGroup , " === ", " sumUserCountInloop :", sumUserCountInloop);
-                            if (countGroup === sumUserCountInloop) {
+                            console.log("countGroup :", countGroup, " === ", " sumNumMember :", sumNumMember);
+                            if (countGroup === sumNumMember) {
                                 let arrUer = await getUserGroup(event.source.groupId)
                                 if (arrUer !== 0) {
-                                    await replyTableInGroup(event.replyToken, arrUer, arrLoopFlex)
+                                    await replyTableInGroup(event.replyToken, arrUer, arrLoopTable)
                                 }
                             } else {
                                 await util.reply(event.replyToken, [{
                                     type: "text",
-                                    text: "จำนวนสมาชิกในโต๊ะไม่ถูกต้อง \ สมาชิกในโต๊ะปัจจุบัน : " + countGroup + "  แต่ผลรวมท่านได้ : " + sumUserCountInloop + " \n\n กลับไปกรอกใหม่ให้ถูกต้อง!"
+                                    text: "จำนวนสมาชิกในโต๊ะไม่ถูกต้อง \ สมาชิกในโต๊ะปัจจุบัน : " + countGroup + "  แต่ผลรวมท่านได้ : " + sumNumMember + " \n\n กลับไปกรอกใหม่ให้ถูกต้อง!"
                                 }]);
                             }
 
@@ -125,22 +131,19 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
                             }]);
                         }
                     }
-
-                     // -----------------------
+                    // -----------------------
                 }
 
             }
-            if (event.type === "join") {
-                if (event.source.type === "group") {
-                    await util.reply(event.replyToken, [flex.welcomeMessage()]);
-                }
-            }
+
+            // remove group
             if (event.type === "leave") {
                 if (event.source.type === "group") {
                     await deleteGroup(event.source.groupId)
                 }
             }
-            
+
+            // create member group
             if (event.type === "memberJoined") {
                 if (event.source.type === "group") {
                     for (const member of event.joined.members) {
@@ -153,8 +156,8 @@ exports.Webhook = functions.region("asia-northeast1").https.onRequest(async (req
 
                 }
             }
-            
-            
+
+            // remove member group
             if (event.type === "memberLeft") {
                 if (event.source.type === "group") {
                     for (const member of event.left.members) {
@@ -204,6 +207,7 @@ const deleteUserGroup = async (userId, groupId) => {
     });
 
 }
+
 // delete Group by groupId
 const deleteGroup = async (groupId) => {
 
@@ -237,74 +241,73 @@ const getUserGroup = async (groupId) => {
     return (arrayUser.length > 1) ? arrayUser : 0
 }
 
-
 // Reply Message and Random User and Table
-const replyTableInGroup = async (replyToken, arrayUser, arrLoopFlex) => {
+const replyTableInGroup = async (replyToken, arrayUser, arrLoopTable) => {
+
+
+
+    // randomize (shuffle)
     let randomUser = arrayUser.sort(function () {
         return 0.5 - Math.random()
     })
+    //----------------------
 
-    var arrTable = []
-    arrTable[0] = {
-        members: []
-    }
-    arrTable[1] = {
-        members: []
-    }
-    arrTable[2] = {
-        members: []
-    }
-    arrTable[3] = {
-        members: []
-    }
-    arrTable[4] = {
-        members: []
-    }
-    arrTable[5] = {
-        members: []
-    }
-    arrTable[6] = {
-        members: []
-    }
-    arrTable[7] = {
-        members: []
-    }
-    arrTable[8] = {
-        members: []
-    }
-    arrTable[9] = {
-        members: []
-    }
-    arrTable[10] = {
-        members: []
-    }
 
-    tableIndex = 0
-    randomUser.forEach((value) => {
-        arrTable[tableIndex].members.push(value)
-        if (arrTable[tableIndex].members.length >= arrLoopFlex[tableIndex]) tableIndex++
-    });
+    // set table
+    // recommend 10-20 table 
+    const countTable = 10
 
-    let nameList = ''
-    let groupNo = 1
-    arrTable.forEach((element, index) => {
+    // array user per table <= table
+    // exm.
+    // แตก 1 1 1 1 1 1 1 1 1 1 => 10 = true
+    // แตก 1 1 1 1 1 1 1 1 1 1 2 => 11 = false
+    if (arrLoopTable.length <= countTable) {
 
-        if (element.members.length > 0 && element.members.length <= 10) {
-            nameList += 'โต๊ะ ' + groupNo + " จำนวน " + element.members.length + " คน"
-            let memberNo = 1
-            element.members.forEach((memberList) => {
-                nameList += " \n " + memberNo + "." + memberList.displayName
-                memberNo++
-            });
-            nameList += "\n ------------------\n "
-            groupNo++
 
+        // create array table
+        var arrTable = []
+        for (let index = 0; index <= countTable; index++) {
+
+            arrTable[index] = {
+                members: []
+            }
         }
+        //-------------
 
-    });
-    nameList += " แตกโต๊ะ แต่ไม่แตกแยก กลับมาแตกด้วยกันใหม่น้า"
-    await util.reply(replyToken, [{
-        type: "text",
-        text: nameList
-    }]);
+
+        // set up member on table 
+        tableIndex = 0
+        randomUser.forEach((value) => {
+            arrTable[tableIndex].members.push(value)
+            if (arrTable[tableIndex].members.length >= arrLoopTable[tableIndex]) tableIndex++
+        });
+        //-------------
+
+
+        // message report member
+        let nameList = ''
+        let groupNo = 1
+        arrTable.forEach((elmTable) => {
+
+            if (elmTable.members.length > 0 && elmTable.members.length <= 10) {
+                nameList += 'โต๊ะ ' + groupNo + " จำนวน " + elmTable.members.length + " คน"
+                let memberNo = 1
+                elmTable.members.forEach((memberList) => {
+                    nameList += " \n " + memberNo + "." + memberList.displayName
+                    memberNo++
+                });
+                nameList += "\n ------------------\n "
+                groupNo++
+
+            }
+
+        });
+        nameList += " แตกโต๊ะ แต่ไม่แตกแยก กลับมาแตกด้วยกันใหม่น้า "
+        await util.reply(replyToken, [{
+            type: "text",
+            text: nameList
+        }]);
+        //-------------
+    }
+
 }
